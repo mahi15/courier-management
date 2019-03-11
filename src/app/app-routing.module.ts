536bd6a1133699625e277b46fc46454e0741ba58
Mahaveed <mahaveerd15@gmail.com>
2019-03-11 23:38:30 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  { path:"login", component:LoginComponent },
+  { path:"admin", component:AdminComponent }
+];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      {
-        path:"",
-        component:HomeComponent
-      },
-      {
-      path:"login",
-        component:LoginComponent
-      },
-      {
-        path:"admin",
-          component:AdminComponent
-        }
-    ])
-  ],
+    RouterModule.forRoot(routes) ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [LoginComponent,AdminComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule,routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    AdminComponent,
-    HomeComponent
+   routingComponents,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatListModule ,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    LayoutModule 
 
   ],
   providers: [],
